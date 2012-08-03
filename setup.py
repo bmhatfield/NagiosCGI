f976a3e651d2af2708348a9848408c5adf7de9a9
Brian Hatfield <bhatfield@brightcove.com>
2012-08-03 19:27:56 -0400

    Add distutils setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='nagcgi',
+      version='1.0',
+      description="Python Implementation of the Nagios CGI URL API",
+      author="Brian Hatfield",
+      author_email="bmhatfield@gmail.com",
+      url="https://github.com/bmhatfield/NagiosCGI",
+      py_modules=['nagcgi']
+      )
